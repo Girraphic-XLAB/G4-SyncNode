1251abe7298416c29d0ecb859c5c9ba6560be1e7
Aiden <68633820+awils27@users.noreply.github.com>
2022-09-20 16:51:09 +1000

    Included new config variables inside the config reader and assembled the perforce sync and force sync commands

--- XLAB OSC Client/ConfigLoader.py
+++ XLAB OSC Client/ConfigLoader.py
@@ -9,5 +9,11 @@ config.read('config.ini')
 #Variables from the configuration file.
 OSCIP = config['OSC']['IncomingIP']
 OSCPORT = int(config['OSC']['IncomingPort'])
-OSCPull = config['OSC']['PullCommand']
+OSCSync = config['OSC']['SyncAddress']
+OSCForceSync = config['OSC']['ForceSyncAddress']
+ProjectDir = config['GENERAL']['ProjectRootDir']
+P4Stream = config['GENERAL']['P4StreamDir']
 
+
+PerforceSync = 'p4 sync -q ' + ProjectDir + '\... #head'
+PerforceForceSync = 'p4 sync -f -q ' + ProjectDir + '\... #head'
