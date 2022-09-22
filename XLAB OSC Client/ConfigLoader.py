e385c9810f9a6c42b22c2369f347d0b32048a4ee
Aiden <68633820+awils27@users.noreply.github.com>
2022-09-23 08:48:28 +1000

    Included more OSC address variables and assembled commands for revert and query

--- XLAB OSC Client/ConfigLoader.py
+++ XLAB OSC Client/ConfigLoader.py
@@ -7,13 +7,28 @@ config.read('config.ini')
 
 
 #Variables from the configuration file.
+#Osc server variables
 OSCIP = config['OSC']['IncomingIP']
 OSCPORT = int(config['OSC']['IncomingPort'])
+
+#OSC button addresses.
 OSCSync = config['OSC']['SyncAddress']
 OSCForceSync = config['OSC']['ForceSyncAddress']
+OSCQuery = config['OSC']['QueryDepo']
+OSCRevert = config['OSC']['RevertDir']
+OSCShaderDelete = config['OSC']['DeleteShaders']
+OSCSanatise = config['OSC']['SanatiseDir']
+
+#Enable debugging variable
+EnableDebugging = config['GENERAL']['DebugLogging']
+
+#Project directory for P4 Commands.
 ProjectDir = config['GENERAL']['ProjectRootDir']
-P4Stream = config['GENERAL']['P4StreamDir']
 
-
+#Assembled P4 comands to run sync and others.
 PerforceSync = 'p4 sync -q ' + ProjectDir + '\...#head'
 PerforceForceSync = 'p4 sync -f -q ' + ProjectDir + '\...#head'
+PerforceQuery = 'p4 sync -n -q ' + ProjectDir + '\...#head'
+PerforceRevert = 'p4 revert ' + ProjectDir + '\...'
+
+#Other assembled commands
