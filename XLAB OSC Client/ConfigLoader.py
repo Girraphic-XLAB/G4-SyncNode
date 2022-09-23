3ca05658b2b9dad303e5cc01b101577a1be492ab
Aiden <68633820+awils27@users.noreply.github.com>
2022-09-23 15:11:20 +1000

    New variables in config for launching Unreal and both Uproject and Unreal directories

--- XLAB OSC Client/ConfigLoader.py
+++ XLAB OSC Client/ConfigLoader.py
@@ -18,15 +18,23 @@ OSCQuery = config['OSC']['QueryDepo']
 OSCRevert = config['OSC']['RevertDir']
 OSCShaderDelete = config['OSC']['DeleteShaders']
 OSCSanatise = config['OSC']['SanatiseDir']
+LaunchUnreal = config['OSC']['LaunchUnreal']
+
 
 #Enable debugging variable
 EnableDebugging = config['GENERAL']['DebugLogging']
 
 #Project directory for P4 Commands.
 ProjectDir = config['GENERAL']['ProjectRootDir']
+UProject = config['GENERAL']['Uproject']
+UnrealDir = config['GENERAL']['UnrealDirectory']
 
 #Assembled P4 comands to run sync and others.
 PerforceSync = 'p4 sync -q ' + ProjectDir + '\...#head'
 PerforceForceSync = 'p4 sync -f -q ' + ProjectDir + '\...#head'
 PerforceQuery = 'p4 sync -n -q ' + ProjectDir + '\...#head'
 PerforceRevert = 'p4 revert ' + ProjectDir + '\...'
+
+
+#Unreal Launch Command
+UELaunch =  UnrealDir + ' ' + ProjectDir + '\\' + UProject + ' -game'
